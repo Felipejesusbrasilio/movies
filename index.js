d4e1ef3f5d158cbd87068affefff426b364130e7
FELIPE DE JESUS BRASILIO DA COSTA <69780275+Felipejesusbrasilio@users.noreply.github.com>
2023-05-22 23:57:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -233,7 +233,7 @@ token:hash
 
 });
 
-const code = _pix.Pix("lipe.tr@hotmail.com", "felipe de jesus brasilio da costa", "são paulo", "100", "Compra do metodo english movies br");
+const code = _pix.Pix("lipe.tr@hotmail.com", "felipe de jesus brasilio da costa", "São paulo", "100", "Compra do metodo english movies br");
 
 code.then(result=>{
 
